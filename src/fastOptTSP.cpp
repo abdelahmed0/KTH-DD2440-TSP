9cc9ba673b072afac4a1e2e5a777f873cad4ac97
Abdel Ahmed <abdelahm@kth.se>
2023-11-07 18:55:51 +0100

    Implemented fastThreeOpt, performs bad on kattis but well locally

--- src/fastOptTSP.cpp
+++ src/fastOptTSP.cpp
@@ -134,7 +134,7 @@ inline vector<uint32_t> greedy(Matrix& m) {
 inline void fastTwoOpt(Matrix& d, Matrix& nbhd,
                        vector<uint32_t>& tour, vector<uint32_t>& whichSlot,
                        uint32_t minLink, uint32_t& maxLink,
-                       chrono::steady_clock::time_point startTime, uint16_t timeLimit) {
+                       chrono::steady_clock::time_point startTime, uint32_t timeLimit) {
     size_t N = d.rows();
     bool improved = true;
 
@@ -172,12 +172,131 @@ inline void fastTwoOpt(Matrix& d, Matrix& nbhd,
     }
 }
 
+/**
+ * Sorts three edges according to their position in the tour, given by the indices
+ */
+inline void sortInTourOrder(vector<uint32_t>& cities, vector<size_t>& ind) {
+    if (ind[0] < ind[4] && ind[0] > ind[2] 
+        || ind[0] > ind[2] && ind[2] > ind[4]
+        || ind[0] < ind[4] && ind[2] > ind[4]) {
+            vector<uint32_t> tmp_c = cities;
+            cities[0] = tmp_c[2]; cities[1] = tmp_c[3];
+            cities[2] = tmp_c[0]; cities[3] = tmp_c[1];
+            // cities[4] and cities[5] do not change
+            vector<size_t> tmp_i = ind;
+            ind[0] = tmp_i[2]; ind[1] = tmp_i[3];
+            ind[2] = tmp_i[0]; ind[3] = tmp_i[1];
+            // and their indices neither
+        }
+}
+
+/**
+ * Performs fast the 3-opt local optimization algorithm from the paper
+ * "Large-Step Markov Chains for the Traveling Salesman Problem"
+ */
+inline void fastThreeOpt(Matrix& d, Matrix& nbhd, 
+                     vector<uint32_t>& tour, vector<uint32_t>& whichSlot, 
+                     uint32_t minLink, uint32_t& maxLink, 
+                     chrono::steady_clock::time_point startTime, uint32_t timeLimit) {
+    const size_t N = d.rows();
+
+    bool improved = true;
+    while (improved) {
+        improved = false;
+
+        if (timeOver(startTime, timeLimit))
+            return; 
+
+        // for each edge (m1, n1)
+        for (size_t n1_i = 0; n1_i < N; ++n1_i) {
+            size_t m1_i = (n1_i - 1 + N) % N;
+            uint32_t m1 = tour[m1_i];
+            uint32_t n1 = tour[n1_i];
+
+            // for each edge (m2, n2) where n2 is choosen as the  k-nearest neighbor of m1
+            for (size_t k1 = 0; k1 < nbhd.cols(); ++k1) {
+                uint32_t n2_i = whichSlot[nbhd.at(m1, k1)];
+                size_t m2_i = (n2_i + N - 1) % N; 
+                uint32_t m2 = tour[m2_i];
+                uint32_t n2 = tour[n2_i];
+
+                bool newBestMove = false;
+
+                if (d.at(m1, n2) + 2 * minLink > d.at(m1, n1) + 2 * maxLink)
+                    break; // next (m1, n1) edge
+
+                if (m1 == m2 
+                    || m2 == n1
+                    || d.at(m1, n2) + 2 * minLink > d.at(m1, n1) + d.at(m2, n2) + maxLink)
+                    continue; // next (m2, n2) edge
+
+                // for each (m3, n3) where n3 is chosen as the k-closest neighbor of m2
+                for (size_t k2 = 0; k2 < nbhd.cols(); ++k2) {
+                    uint32_t n3_i = whichSlot[nbhd.at(m1, k2)];
+                    size_t m3_i = (n3_i + N - 1) % N;
+                    uint32_t m3 = tour[m3_i];
+                    uint32_t n3 = tour[n3_i];
+
+                    if (d.at(m1, n2) + d.at(n1, n3) + minLink > d.at(m1, n1) + d.at(m2, n2) + maxLink)
+                        break; // next (m2, n2) edge
+
+                    if (n3 == n2 || m3 == m1
+                        || n3 == m2 ||  m3 == n2
+                        || m3 == n1)
+                        continue; // next (m3, n3) edge
+
+                    // sort cities according to their occurence in the tour
+                    // and keep track of their indices for tour segment reversal
+                    vector<uint32_t> C = {m1, n1, m2, n2, m3, n3};
+                    vector<size_t> I = {m1_i, n1_i, m2_i, n2_i, m3_i, n3_i};
+                    sortInTourOrder(C, I);
+
+                    uint32_t oldDelta = d.at(C[0], C[1]) + d.at(C[2], C[3]) + d.at(C[4], C[5]);
+
+                    // Try the two topologically different moves
+                    // which correspond to four different ordered cases 
+                    if (d.at(C[1], C[4]) + d.at(C[3], C[0]) + d.at(C[5], C[2]) < oldDelta) {
+                        newBestMove = true;
+                        reverseTourSegment(tour, whichSlot, I[0], I[5]);
+                        reverseTourSegment(tour, whichSlot, I[1], I[2]);
+                        reverseTourSegment(tour, whichSlot, I[3], I[4]);
+                        maxLink = max(maxLink, max(max(d.at(C[1], C[4]), d.at(C[3], C[0])), d.at(C[5], C[2])));
+                    } else if (d.at(C[3], C[0]) + d.at(C[5], C[1]) + d.at(C[2], C[4]) < oldDelta) {
+                        newBestMove = true;
+                        reverseTourSegment(tour, whichSlot, I[5], I[0]);
+                        reverseTourSegment(tour, whichSlot, I[3], I[4]);
+                        maxLink = max(maxLink, max(max(d.at(C[3], C[0]), d.at(C[5], C[1])), d.at(C[2], C[4])));
+                    }  else if (d.at(C[1], C[3]) + d.at(C[4], C[0]) + d.at(C[5], C[2]) < oldDelta) {
+                        newBestMove = true;
+                        reverseTourSegment(tour, whichSlot, I[5], I[0]);
+                        reverseTourSegment(tour, whichSlot, I[1], I[2]);
+                        maxLink = max(maxLink, max(max(d.at(C[1], C[3]), d.at(C[4], C[0])), d.at(C[5], C[2])));
+                    } else if (d.at(C[0], C[2]) + d.at(C[1], C[4]) + d.at(C[3], C[5]) < oldDelta) {
+                        newBestMove = true;
+                        reverseTourSegment(tour, whichSlot, I[1], I[2]);
+                        reverseTourSegment(tour, whichSlot, I[3], I[4]);
+                        maxLink = max(maxLink, max(max(d.at(C[0], C[2]), d.at(C[1], C[4])), d.at(C[3], C[5])));
+                    }
+
+                    if (newBestMove) {
+                        improved = true;
+                        break;
+                    }
+                }
+                if (newBestMove) {
+                    break;
+                }
+            }
+        }
+    }
+}
+
 
 int main(int argc, char *argv[]) {
     chrono::steady_clock::time_point startTime = chrono::steady_clock::now();
-    const uint16_t timeLimit = 1950;
-    const uint16_t twoOptTimeLimit = 1950;
-    const size_t K_NEAREST = 140;
+    const uint32_t timeLimit = 1900;
+    const uint32_t twoOptTimeLimit = 200;
+    const size_t K_NEAREST = 15;
 
 #if TESTING
     fstream testFile;
@@ -207,6 +326,7 @@ int main(int argc, char *argv[]) {
     uint32_t maxLink = *max_element(begin(tour), end(tour));
 
     fastTwoOpt(distanceMatrix, nbhd, tour, whichSlot, minLink, maxLink, startTime, twoOptTimeLimit);
+    fastThreeOpt(distanceMatrix, nbhd, tour, whichSlot, minLink, maxLink, startTime, timeLimit);
 
     for (int i = 0; i < tour.size(); ++i) {
         cout << tour[i] << endl;
